339dba35dfc74e3ca40ffd4ebba42d6972ee522d
frodo33 <tomaszgwozdz95@gmail.com>
2025-02-19 13:59:23 +0100

    feat: some example

--- frontend/src/pages/AuthPages/Login/Login.page.tsx
+++ frontend/src/pages/AuthPages/Login/Login.page.tsx
@@ -1,16 +1,12 @@
 import type { FC } from "react";
-import { Box, Button, Paper, Typography, useColorScheme, useTheme } from "@mui/material";
+import { Box, Paper, Typography, useColorScheme } from "@mui/material";
+import { FaLock } from "react-icons/fa6";
 
-// button
-// inputs
-// terms
-// button do register
-// button do forgot-password
-// 
+import { TextField } from "@/components/form/TextField/TextField.component";
 
 export const LoginPage: FC = () => {
   const { mode, setMode } = useColorScheme()
-  const theme = useTheme()
+
   return (
     <Box
       display="flex"
@@ -19,25 +15,46 @@ export const LoginPage: FC = () => {
       justifyContent="center"
 
     >
-      <Paper
-        sx={{
-          minWidth: "550px",
-
-          p: 3,
-          borderRadius: theme.shape.lg,
-          boxShadow: theme.boxShadows.md,
-        }}
-      >
+      <Paper>
         <button onClick={() => setMode("light")}>light</button>
         <button onClick={() => setMode("dark")}>dark</button>
         <Typography variant="h1">Login</Typography>
-        <Typography variant="h1">Login</Typography>
-        <Typography variant="h1">Login</Typography>
-        <Typography variant="h1">Login</Typography>
-        <Typography variant="h1">Login</Typography>
 
-        <Button variant="contained">Save</Button>
-        <Button variant="outlined">Log in</Button>
+        <Box>
+          <TextField
+            label="Test"
+            startAdornment={<FaLock />}
+          />
+        </Box>
+
+        <Box>
+          <TextField
+            label="Username"
+            helperText="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam labore obcaecati error est enim, laboriosam rerum voluptate nostrum velit qui."
+            error={true}
+            endAdornment={<FaLock />}
+            placeholder="Test"
+          />
+        </Box>
+
+        <Box>
+          <TextField
+            label="Password"
+            size="small"
+            helperText="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam labore obcaecati error est enim, laboriosam rerum voluptate nostrum velit qui."
+            placeholder="Test"
+          />
+        </Box>
+
+        <Box>
+          <TextField
+            label="Test"
+            size="small"
+            helperText="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam labore obcaecati error est enim, laboriosam rerum voluptate nostrum velit qui."
+            disabled
+            placeholder="Test"
+          />
+        </Box>
       </Paper>
     </Box>
   )
